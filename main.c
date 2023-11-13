657e4f9f71a61e81e50c68fa3da4570bdf232ff3
brucemack <bruce@mackinnon.com>
2023-11-13 14:04:50 +0000

    First commit

---
+++ main.c
@@ -0,0 +1,50 @@
+/*
+# Make sure that the PICO_SDK_PATH is set properly
+
+cd /home/bruce/pico/hello-pico
+# You need main.c and CMakeLists.txt
+cp ../pico-sdk/external/pico_sdk_import.cmake .
+mkdir build
+cd build
+cmake ..
+make
+# Connect the PI PICO to USB while pressing the BOOTSEL button.
+# And then:
+cp test.uf2 /media/bruce/RPI-RP2
+
+# Make sure the SWD is connected properly:
+# GPIO24 (Pin 18) to SWDIO
+# GPIO25 (Pin 22) to SWCLK
+# GND (Pin 20) to SWGND
+
+# Use this command to flash:
+openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program test.elf verify reset exit"
+
+# Looking at the serial port:
+minicom -b 115200 -o -D /dev/ttyACM0
+*/
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+ 
+ const uint LED_PIN = 25;
+ 
+ int main() {
+ 
+    bi_decl(bi_program_description("This is a test binary."));
+    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (1) {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(1000);
+        gpio_put(LED_PIN, 1);
+        puts("Hello World\n");
+        sleep_ms(1000);
+    }
+}
+
