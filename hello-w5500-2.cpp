83ee9df8f53ff09151c4d8cc7d5b53bea7991759
brucemack <bruce@mackinnon.com>
2024-03-02 21:51:31 +0000

    Improved DHCP detection

--- hello-w5500-2.cpp
+++ hello-w5500-2.cpp
@@ -155,7 +155,7 @@ static void set_Sn_RX_RD(spi_inst_t* spi, uint8_t socket, uint16_t v) {
     set_Sn_reg16(spi, socket, 0x28, v);
 }
 
-static uint8_t get_reg8(spi_inst_t* spi, uint16_t offset) {
+static uint8_t get_reg8(spi_inst_t* spi, uint8_t block, uint16_t offset) {
     uint8_t buf[3];
     // S0_CR Setup
     gpio_put(SPI0_CSN_PIN, 0);
@@ -164,7 +164,7 @@ static uint8_t get_reg8(spi_inst_t* spi, uint16_t offset) {
     buf[0] = (offset & 0xff00) >> 8;
     buf[1] = (offset & 0x00ff);
     // Control
-    buf[2] = 0b00000 | 0b0'00;
+    buf[2] = ((block & 0b11111) << 3) | 0b0'00;
     spi_write_blocking(spi0, buf, 3);
     spi_read_blocking(spi0, 0, buf, 1);
     gpio_put(SPI0_CSN_PIN, 1);
@@ -173,20 +173,7 @@ static uint8_t get_reg8(spi_inst_t* spi, uint16_t offset) {
 }    
 
 static uint8_t get_Sn_reg8(spi_inst_t* spi, uint8_t socket, uint16_t offset) {
-    uint8_t buf[3];
-    // S0_CR Setup
-    gpio_put(SPI0_CSN_PIN, 0);
-    asm volatile("nop \n nop \n nop");
-    // Offset
-    buf[0] = (offset & 0xff00) >> 8;
-    buf[1] = (offset & 0x00ff);
-    // Control
-    buf[2] = ((socket * 4 + 1) << 3) | 0b0'00;
-    spi_write_blocking(spi0, buf, 3);
-    spi_read_blocking(spi0, 0, buf, 1);
-    gpio_put(SPI0_CSN_PIN, 1);
-    asm volatile("nop \n nop \n nop");
-    return buf[0];
+    return get_reg8(spi, socket * 4 + 1, offset);
 }    
 
 static uint16_t get_Sn_reg16(spi_inst_t* spi, uint8_t socket, uint16_t offset) {
@@ -207,15 +194,15 @@ static uint16_t get_Sn_reg16(spi_inst_t* spi, uint8_t socket, uint16_t offset) {
 }    
 
 static uint8_t get_MR(spi_inst_t* spi) {
-    return get_reg8(spi, 0);
+    return get_reg8(spi, 0, 0);
 }    
 
 static uint8_t get_SIR(spi_inst_t* spi) {
-    return get_reg8(spi, 0x17);
+    return get_reg8(spi, 0, 0x17);
 }    
 
 static uint8_t get_PHYCFGR(spi_inst_t* spi) {
-    return get_reg8(spi, 0x2e);
+    return get_reg8(spi, 0, 0x2e);
 }    
 
 static void get_Sn_RXBUF(spi_inst_t* spi, uint8_t socket, uint16_t offset, uint8_t* buf2, uint16_t len2) {
@@ -311,12 +298,31 @@ uint16_t computeCs(uint8_t* data, uint32_t dataLen) {
     return ~result;
 }
 
-struct Ethernet2Header {
+#pragma pack(1)
+struct EthHeader {
+
     uint8_t dstMAC[6];
     uint8_t srcMAC[6];
     uint16_t typ;
+
+    bool isIP4() const { return ntohs(typ) == 0x0800; }
+    
+    bool isBroadcast() const { 
+        return 
+            dstMAC[0] == 0xff &&
+            dstMAC[1] == 0xff &&
+            dstMAC[2] == 0xff &&
+            dstMAC[3] == 0xff &&
+            dstMAC[4] == 0xff &&
+            dstMAC[5] == 0xff;
+    }
+
+    bool isDstMAC(const uint8_t* mac) {
+        return memcmp(mac, dstMAC, 6) == 0;
+    }
 };
 
+#pragma pack(1)
 struct ARP {
     uint16_t hwTyp;
     uint16_t protoTyp;
@@ -329,6 +335,7 @@ struct ARP {
     uint8_t tgtIP4[4];
 };
 
+#pragma pack(1)
 struct IP4Header {
     // Ver 4, Hdr Len = 20
     uint8_t verHdrLen = 0x45;
@@ -347,6 +354,10 @@ struct IP4Header {
     uint16_t hdrCs = 0;
     uint8_t srcIP4[4];
     uint8_t dstIP4[4];
+
+    bool isUDP() const {
+        return proto == 17;
+    }
 
     void populateHdrCs() {
         hdrCs = 0;
@@ -368,6 +379,8 @@ struct UDPHeader {
         hdrCs = 0;
         hdrCs = htons(computeCs((uint8_t*)this, sizeof(UDPHeader)));
     }
+
+    uint16_t getDstPort() const { return ntohs(dstPort); }
 };
 
 #pragma pack(1)
@@ -394,7 +407,7 @@ struct DHCPHeader {
 
 struct DHCPRequest {
 
-    Ethernet2Header eth;
+    EthHeader eth;
     IP4Header ip;
     UDPHeader udp;
     DHCPHeader dhcp;
@@ -470,54 +483,151 @@ void rxBufferPeek(uint8_t* buf, uint32_t len) {
     __dsb();
 }
 
-static void rxHandler(uint gpio, uint32_t events) {
+static void intHandler(uint gpio, uint32_t events) {
 
     // Now we need to clear the interrupt flags immediately to 
     // make sure we don't lose anything.
+    //
     gpio_acknowledge_irq(W5500_INT_PIN, events);
     set_SIR(spi0, 0);
+
+    // Capture event that triggered the interrupt
+    uint8_t ir = get_Sn_IR(spi0, 0);
+    // NOTE: We need to investigate the race-condition that exists
+    // here when an interrupt happens on the S5500 chip between the 
+    // time we read the IR and the time we clear the IR.  Can we
+    // use an atomic read/write here?
     set_Sn_IR(spi0, 0, 0xff);
 
-    // Pull data from the network as agressively as possible
-    uint16_t rx_rsr = get_Sn_RX_RSR(spi0, 0);
-
-    if (rx_rsr) {
-
-        uint16_t rx_rd = get_Sn_RX_RD(spi0, 0);            
-        // Compute the reads, possibly in two parts if we are 
-        // close to the end of the W5500 buffer
-        uint16_t offset0 = rx_rd & blockMask;
-        uint16_t len0 = std::min(rx_rsr, (uint16_t)(blockSize - offset0));
-        uint16_t offset1 = 0;
-        uint16_t len1 = rx_rsr - len0;
-
-        uint8_t buf[blockSize];
-
-        get_Sn_RXBUF(spi0, 0, offset0, buf, len0);
-        if (len1 > 0) {
-            get_Sn_RXBUF(spi0, 0, offset1, buf + len0, len1);
-        }
-
-        // Move into the software buffer
-        for (uint32_t i = 0; i < len0 + len1; i++) {
-            rxBuffer[rxBufferWR & rxBufferMask] = buf[i];
-            // Check for the overflow case
-            if (((rxBufferWR + 1) & rxBufferMask) == (rxBufferRD & rxBufferMask)) {
-                rxBufferOF++;
-            } else {
-                rxBufferWR++;
-                // TODO: WRAP CASE
-            }
-        }
-
-        // Note that the management of RX_RD is independent of the 
-        // size of the RX buffer.  The addressing just pretends
-        // everything is 64K. This is expected to wrap at 0xffff!  
-        rx_rd += rx_rsr;
-        set_Sn_RX_RD(spi0, 0, rx_rd);
-        // Tell the controller that we've done a receive
-        set_Sn_CR(spi0, 0, 0x40);
+    if (ir & 0b001'0000) {
+        cout << "Send OK" << endl;
     }
+
+    if (ir & 0b000'0100) {
+
+        // Pull data from the network as agressively as possible
+        uint16_t rx_rsr = get_Sn_RX_RSR(spi0, 0);
+
+        if (rx_rsr) {
+
+            uint16_t rx_rd = get_Sn_RX_RD(spi0, 0);            
+            // Compute the reads, possibly in two parts if we are 
+            // close to the end of the W5500 buffer
+            uint16_t offset0 = rx_rd & blockMask;
+            uint16_t len0 = std::min(rx_rsr, (uint16_t)(blockSize - offset0));
+            uint16_t offset1 = 0;
+            uint16_t len1 = rx_rsr - len0;
+
+            uint8_t buf[blockSize];
+
+            get_Sn_RXBUF(spi0, 0, offset0, buf, len0);
+            if (len1 > 0) {
+                get_Sn_RXBUF(spi0, 0, offset1, buf + len0, len1);
+            }
+
+            // Move into the software buffer
+            for (uint32_t i = 0; i < len0 + len1; i++) {
+                rxBuffer[rxBufferWR & rxBufferMask] = buf[i];
+                // Check for the overflow case
+                if (((rxBufferWR + 1) & rxBufferMask) == (rxBufferRD & rxBufferMask)) {
+                    rxBufferOF++;
+                } else {
+                    rxBufferWR++;
+                    // TODO: WRAP CASE
+                }
+            }
+
+            // Note that the management of RX_RD is independent of the 
+            // size of the RX buffer.  The addressing just pretends
+            // everything is 64K. This is expected to wrap at 0xffff!  
+            rx_rd += rx_rsr;
+            set_Sn_RX_RD(spi0, 0, rx_rd);
+            // Tell the controller that we've done a receive
+            set_Sn_CR(spi0, 0, 0x40);
+        }
+    }
+}
+
+bool sendMessage(spi_inst_t* spi, const uint8_t* d, uint16_t dLen, bool blocking) {
+
+    uint16_t tx_fsr = get_Sn_TX_FSR(spi, 0);            
+    uint16_t tx_wr = get_Sn_TX_WR(spi, 0);            
+
+    if (dLen > tx_fsr) {
+        return false;
+    }
+    else {
+        // It's possible that we'd need to do this in two parts
+        // because of buffer wrap
+        uint16_t offset0 = tx_wr;
+        uint16_t len0 = std::min(dLen, (uint16_t)(blockSize - offset0));
+        uint16_t offset1 = 0;
+        uint16_t len1 = dLen - len0;
+        set_Sn_TXBUF(spi, 0, offset0, d, len0);
+        if (len1 > 0) {
+            set_Sn_TXBUF(spi, 0, offset1, d + len0, len1);
+        }
+        // Adjust pointers.  No regard to wrapping in this logic
+        set_Sn_TX_WR(spi, 0, tx_wr + (len0 + len1));
+        // Tell the controller that we've done a SEND
+        set_Sn_CR(spi, 0, 0x20);
+        // Optionally wait for the SEND command to be executed
+        if (blocking) {
+            // TODO: TIMEOUT
+            while (get_Sn_CR(spi0, 0));
+        }
+        return true;
+    }
+}
+
+void initW5500(spi_inst_t* spi, const uint8_t* myMAC) {
+
+    // Do the formal reset
+    gpio_put(W5500_RST_PIN, 0);
+    //sleep_ms(1);
+    asm volatile("nop \n nop \n nop");
+    gpio_put(W5500_RST_PIN, 1);
+
+    // Long wait 
+    sleep_ms(100);
+
+    // Reset, wait until done
+    set_MR(spi, get_MR(spi) | 0x80);
+    while (get_MR(spi) != 0);
+
+    sleep_ms(250);
+    
+    // Set MAC address
+    set_SHAR(spi, myMAC);
+
+    // PHY RESET, wait until done
+    set_PHYCFGR(spi, get_PHYCFGR(spi0) | 0x80);
+    while ((get_PHYCFGR(spi) & 0x80) == 0);
+    // 100/Full/Manual seems to work
+    set_PHYCFGR(spi, 0xd8);
+    // Wait for link to come up
+    while (get_PHYCFGR(spi) != 0xdf);
+
+    sleep_ms(250);
+
+    // Adjust buffer size up to maximum
+    set_Sn_RXBUF_SIZE(spi, 0, 16);
+    set_Sn_TXBUF_SIZE(spi, 0, 16);
+    // S0_MR Setup (MACRAW)
+    // MAC filter enabled
+    set_Sn_MR(spi, 0, 0b1001'0100);
+    // Enable socket 0 interrupts
+    set_SIMR(spi, 1);
+
+    // S0_CR Setup (OPEN)
+    set_Sn_CR(spi, 0, 0x01);
+    // Wait for status = OPEN
+    while (get_Sn_SR(spi, 0) != 0x42);
+
+    // Clear interrupts
+    set_SIR(spi, 0);
+    set_Sn_IR(spi, 0, 0xff);
+    gpio_set_irq_enabled_with_callback(W5500_INT_PIN, eventMask, true, intHandler);
 }
 
 int main() {
@@ -553,46 +663,9 @@ int main() {
 
     sleep_ms(1);
 
-    // Do the formal reset
-    gpio_put(W5500_RST_PIN, 0);
-    //sleep_ms(1);
-    asm volatile("nop \n nop \n nop");
-    gpio_put(W5500_RST_PIN, 1);
-
-    // Long wait 
-    sleep_ms(100);
-
     // Just made this up
     uint8_t myMAC[6] = { 0xd4, 0xbe, 0xd9, 0xe8, 0xba, 0xbe };
-
-    // Reset, wait until done
-    set_MR(spi0, get_MR(spi0) | 0x80);
-    while (get_MR(spi0) != 0);
-    
-    // Set MAC address
-    set_SHAR(spi0, myMAC);
-
-    // PHY RESET, wait until done
-    set_PHYCFGR(spi0, get_PHYCFGR(spi0) | 0x80);
-    while ((get_PHYCFGR(spi0) & 0x80) == 0);
-    // 100/Full/Manual seems to work
-    set_PHYCFGR(spi0, 0xd8);
-    // Wait for link to come up
-    while (get_PHYCFGR(spi0) != 0xdf);
-
-    // Adjust buffer size up to maximum
-    set_Sn_RXBUF_SIZE(spi0, 0, 16);
-    set_Sn_TXBUF_SIZE(spi0, 0, 16);
-    // S0_MR Setup (MACRAW)
-    // MAC filter enabled
-    set_Sn_MR(spi0, 0, 0b1001'0100);
-    // Enable socket 0 interrupts
-    set_SIMR(spi0, 1);
-
-    // S0_CR Setup (OPEN)
-    set_Sn_CR(spi0, 0, 0x01);
-    // Wait for status = OPEN
-    while (get_Sn_SR(spi0, 0) != 0x42);
+    initW5500(spi0, myMAC);
 
     DHCPRequest req;
     uint16_t reqLen;
@@ -606,7 +679,7 @@ int main() {
         
         // IP4
         req.ip.proto = 17;
-        req.ip.totalLen = htons(sizeof(DHCPRequest) - sizeof(Ethernet2Header));
+        req.ip.totalLen = htons(sizeof(DHCPRequest) - sizeof(EthHeader));
         setZeroIP4(req.ip.srcIP4);
         setBroadcastIP4(req.ip.dstIP4);
 
@@ -622,10 +695,10 @@ int main() {
         req.dhcp.xid = 7;
         req.dhcp.secs = 0;
         // Turn on broadcast?
-        //req.dhcp.flags = htons(0x8000);
+        req.dhcp.flags = htons(0x8000);
         // Unicast - this causes the offer to be sent directly
         // to us instead of being broadcast
-        req.dhcp.flags = htons(0x0000);
+        //req.dhcp.flags = htons(0x0000);
         setZeroIP4(req.dhcp.cIP4);
         setZeroIP4(req.dhcp.yIP4);
         // Next
@@ -659,8 +732,8 @@ int main() {
 
         // Fill in lengths
         reqLen = sizeof(DHCPRequest) - 256 + 18;
-        req.ip.totalLen = htons(reqLen - sizeof(Ethernet2Header));
-        req.udp.totalLen = htons(reqLen - sizeof(Ethernet2Header) - sizeof(IP4Header));
+        req.ip.totalLen = htons(reqLen - sizeof(EthHeader));
+        req.udp.totalLen = htons(reqLen - sizeof(EthHeader) - sizeof(IP4Header));
         // Do this last!
         // Optional?
         //req.udp.populateHdrCs();
@@ -668,32 +741,10 @@ int main() {
     }
 
     // Transmit the packet
-    {
-        uint16_t tx_wr = get_Sn_TX_WR(spi0, 0);            
-        // It's possible that we'd need to do this in two parts
-        // because of buffer wrap
-        uint16_t offset0 = 0;
-        uint16_t len0 = reqLen;
-        uint16_t len1 = 0;
-        // Write the data
-        set_Sn_TXBUF(spi0, 0, offset0, (const uint8_t*)&req, len0);
-        // Adjust pointers.  No regard to wrapping in this logic
-        set_Sn_TX_WR(spi0, 0, tx_wr + (len0 + len1));
-        // Tell the controller that we've done a SEND
-        set_Sn_CR(spi0, 0, 0x20);
-        // Wait for the SEND command to be executed
-        while (get_Sn_CR(spi0, 0));
-    
-        prettyHexDump((const uint8_t*)&req, len0, cout, true);
-    }
+    sendMessage(spi0, (const uint8_t*)&req, reqLen, false);
 
     // Receive loop
     int block = 16;
-
-    // Clear interrupts
-    set_SIR(spi0, 0);
-    set_Sn_IR(spi0, 0, 0xff);
-    gpio_set_irq_enabled_with_callback(W5500_INT_PIN, eventMask, true, rxHandler);
 
     while (block > 0) {     
 
@@ -709,35 +760,41 @@ int main() {
         if (rxBufferAvailable() < len) {
             continue;
         }
+
         // Pull out a complete message
         uint8_t buf[blockSize] __attribute__ ((aligned(32)));
         // (Discard length)
         rxBufferRead(lenBuf, 2);
         len -= 2;
         rxBufferRead(buf, len);
-        
-        //cout << "Got " << len << endl;
-        //cout << "--------------------------------------------------" << endl;
-        //prettyHexDump(buf, len, cout, true);
 
         // Process packet
-        const Ethernet2Header* ethHdr = (const Ethernet2Header*)buf;
+        auto ethHdr = (const EthHeader*)buf;
+        if (ethHdr->isIP4()) {
+            cout << "A" << endl;
+            auto ipHdr = (const IP4Header*)(buf + sizeof(EthHeader));
+            if (ipHdr->isUDP()) {
+                cout << "B" << endl;
+                auto udpHdr = (const UDPHeader*)(buf + sizeof(EthHeader) + sizeof(IP4Header));
+                if (udpHdr->getDstPort() == 68) {
 
-        if (buf[35] == 0x43 || buf[35] == 0x44) {
+                    char buf2[20];
+                    formatMAC(ethHdr->dstMAC, buf2);
+                    cout << "DST: " << buf2 << endl;
+                    formatMAC(ethHdr->srcMAC, buf2);
+                    cout << "SRC: " << buf2 << endl;
 
-            char buf2[20];
-            formatMAC(ethHdr->dstMAC, buf2);
-            cout << "DST: " << buf2 << endl;
-            formatMAC(ethHdr->srcMAC, buf2);
-            cout << "SRC: " << buf2 << endl;
-
-            cout << "--------------------------------------------------" << endl;
-            prettyHexDump(buf, len, cout, true);
+                    cout << "--------------------------------------------------" << endl;
+                    prettyHexDump(buf, len, cout, true);
+                }
+            }
         }
 
         block--;
     }
 
+    cout << "OF " << rxBufferOF << endl;
+    cout << "RSR " << get_Sn_RX_RSR(spi0, 0) << endl;
     cout << "Done" << endl;
 
     while(true) {
